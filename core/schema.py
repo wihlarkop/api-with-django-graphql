c1397326c5a9e25bc806e0bea0d101029a5770c6
wihlarkop <wihlarko26@gmail.com>
2021-05-25 17:34:55 +0700

    (update) add graphql for add todo

--- core/schema.py
+++ core/schema.py
@@ -1,14 +1,14 @@
 import graphene
 
-from todoapi.schema import TodoQuery
+from todoapi.schema import TodoQuery, TodoMutation
 
 
 class Query(TodoQuery, graphene.ObjectType):
     pass
 
 
-# class Mutation(graphene.Mutation):
-#     pass
+class Mutation(TodoMutation, graphene.ObjectType):
+    pass
 
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- todoapi/schema.py
+++ todoapi/schema.py
@@ -10,8 +10,29 @@ class TodoType(DjangoObjectType):
         fields = ('id', 'title', 'status')
 
 
+class AddTodo(graphene.Mutation):
+    id = graphene.Int()
+    title = graphene.String()
+    status = graphene.String()
+
+    class Arguments:
+        title = graphene.String()
+
+    def mutate(root, info, title):
+        todo = Todo(
+            title=title
+        )
+        todo.save()
+
+        return AddTodo(id=todo.id, title=todo.title, status=todo.status)
+
+
 class TodoQuery(graphene.ObjectType):
     all_todos = graphene.List(TodoType)
 
     def resolve_all_todos(root, info):
         return Todo.objects.all()
+
+
+class TodoMutation(graphene.ObjectType):
+    add_todo = AddTodo.Field()
