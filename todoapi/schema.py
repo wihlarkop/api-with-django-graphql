465f2efb1231c265baabc9cfde48c8fe8091cd5d
wihlarkop <wihlarko26@gmail.com>
2021-05-28 17:02:08 +0700

    (update) add graphql for update status todo

--- todoapi/schema.py
+++ todoapi/schema.py
@@ -10,6 +10,11 @@ class TodoType(DjangoObjectType):
         fields = ('id', 'title', 'status')
 
 
+class StatusEnum(graphene.Enum):
+    DONE = 'Done'
+    ON_PROCESS = 'On Process'
+
+
 class AddTodo(graphene.Mutation):
     id = graphene.Int()
     title = graphene.String()
@@ -18,6 +23,7 @@ class AddTodo(graphene.Mutation):
     class Arguments:
         title = graphene.String()
 
+    @staticmethod
     def mutate(root, info, title):
         todo = Todo(
             title=title
@@ -27,12 +33,45 @@ class AddTodo(graphene.Mutation):
         return AddTodo(id=todo.id, title=todo.title, status=todo.status)
 
 
+class UpdateStatusTodo(graphene.Mutation):
+    id = graphene.Int()
+    title = graphene.String()
+    status = graphene.String()
+
+    class Arguments:
+        id = graphene.Int()
+        status = StatusEnum()
+
+    @staticmethod
+    def mutate(root, info, id, status):
+        todo = Todo.objects.get(pk=id)
+        todo.status = status
+        todo.save()
+
+
+class DeleteTodo(graphene.Mutation):
+    id = graphene.Int()
+
+    class Arguments:
+        id = graphene.Int()
+
+    @staticmethod
+    def mutate(root, info, id):
+        todo = Todo.objects.get(pk=id)
+        todo.delete()
+
+        return None
+
+
 class TodoQuery(graphene.ObjectType):
     all_todos = graphene.List(TodoType)
 
+    @staticmethod
     def resolve_all_todos(root, info):
         return Todo.objects.all()
 
 
 class TodoMutation(graphene.ObjectType):
     add_todo = AddTodo.Field()
+    update_status_todo = UpdateStatusTodo.Field()
+    delete_todo = DeleteTodo.Field()
