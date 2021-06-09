1a8994408324cebc1129191f7e4889399fae8414
wihlarkop <wihlarko26@gmail.com>
2021-06-09 19:11:38 +0700

    (update)docker compose serve static

--- core/settings.py
+++ core/settings.py
@@ -115,5 +115,9 @@ USE_L10N = True
 USE_TZ = False
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,15 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
 from graphene_django.views import GraphQLView
 
+from core import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
